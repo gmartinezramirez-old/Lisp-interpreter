2c212d194ebc385246c78de3abc37a9588bb83e3
Gonzalo Martinez <gonzalo.martinez.ra@gmail.com>
2017-05-21 15:32:57 -0400

    ADD: loop for calling REP, also change prompt displayed

--- src/main.cc
+++ src/main.cc
@@ -6,19 +6,25 @@
 #include <sstream>
 
 struct Token {
-    enum class Type {
-	NONE,
-	LIST_START,
-	LIST_END,
-	ATOM_STRING,
-	ATOM_NUMERIC,
-	ATOM_SYMBOL,
-	QUOTE,
+	enum class Type {
+		NONE,
+		LIST_START,
+		LIST_END,
+		ATOM_STRING,
+		ATOM_NUMERIC,
+		ATOM_SYMBOL,
+		QUOTE,
      } type;
-    std::string content;
+	std::string content;
  };
 
 int main(int argc, char **argv) {
-    std::string prompt = "\x1b[1;36m" "Lisp-Interpreter" "\x1b[0m" "> ";
-    return 0;
+	std::string prompt = "Lisp-Interpreter> ";
+	std::string input;
+
+	while (1) {
+    	std::cout << rep(input) << "\n";
+    }
+
+	return 0;
 }
